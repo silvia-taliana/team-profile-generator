58ffa0a7bc9eae5e586119feb99c24ae1c97bf08
Silvia Taliana <silviataliana2@gmail.com>
2021-02-20 09:56:50 +1030

    fixed name test

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 const Employee = require("./lib/Employee");
+const inquirer = require("inquirer");
 
 // Initialize a new employee object
 const employee = new Employee();
+
+const questions = [
+    {
+        type: 'input',
+        message: 'What is the name of the employee?',
+        name: 'name'
+    }
+];
+
+function Employee() {
+    inquirer.prompt(questions)
+        .then((answer) => console.log(answer))
+};
+
 
 // Initialize app
 employee.init();
--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -3,10 +3,10 @@ const Employee = require("../lib/Employee");
 describe("Employee", () => {
     describe("Initialization", () => {
         it("should create an object with a name if provided valid arguments", () => {
-            const employee = "Sarah";
+            const employee = new Employee("Sarah", "1", "sarah@email");
 
             // Verify that the new object has the correct properties
-            expect(employee).toEqual("Sarah");
+            expect(employee.name).toEqual("Sarah");
         });
     });
 });
