1c2a604fc04c6c42c3a029dd54ebbedb97f70141
Silvia Taliana <silviataliana2@gmail.com>
2021-02-20 10:28:20 +1030

    fixed employee class and question prompt

--- index.js
+++ index.js
@@ -1,22 +1,33 @@
+// retrieving other files
 const Employee = require("./lib/Employee");
+
+// retrieving dependencies 
 const inquirer = require("inquirer");
 
-// Initialize a new employee object
-const employee = new Employee();
-
+// list of questions for the user 
 const questions = [
     {
         type: 'input',
         message: 'What is the name of the employee?',
         name: 'name'
+    },
+    {
+        type: 'input',
+        message: 'What is the employee\'s id number?',
+        name: 'id'
+    },
+    {
+        type: 'input',
+        message: 'What is the employee\'s email address?',
+        name: 'email'
     }
 ];
 
-function Employee() {
+// function to start asking the user questions
+function init() {
     inquirer.prompt(questions)
         .then((answer) => console.log(answer))
 };
 
-
 // Initialize app
-employee.init();
+init();
