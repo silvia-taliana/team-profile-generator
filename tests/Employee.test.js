c97c25ad74ab894fb556f2132723d45923103e83
Silvia Taliana <silviataliana2@gmail.com>
2021-02-21 10:44:43 +1030

    adding id and email tests

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -8,6 +8,20 @@ describe("Employee", () => {
             // Verify that the new object has the correct properties
             expect(employee.name).toEqual("Sarah");
         });
+
+        it("should create an object with a number if provided valid arguments", () => {
+            const employee = new Employee("Sarah", "1", "sarah@email");
+
+            // Verify that the new object has the correct properties
+            expect(employee.id).toEqual("1");
+        });
+
+        it("should create an object with an email address if provided valid arguments", () => {
+            const employee = new Employee("Sarah", "1", "sarah@email");
+
+            // Verify that the new object has the correct properties
+            expect(employee.email).toEqual("sarah@email");
+        });
     });
 });
 
